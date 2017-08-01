0fdd3171be83a70bba8a3a330ba28431700ddd6c
Yu Iijima <y.iijima@nddhq.co.jp>
2017-08-02 00:58:47 +0900

    SoftModem通信に必要なソースコードを追加

---
+++ Peer/Peer/FSKModemConfig.h
@@ -0,0 +1,27 @@
+/*
+ *  FSKModemConfig.h
+ *
+ *  Created by arms22 on 10/05/06.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#define FREQ_LOW  630
+#define FREQ_HIGH 1260
+#define BAUD  126
+
+//#define FREQ_LOW  1575
+//#define FREQ_HIGH 3150
+//#define BAUD  315
+
+//#define FREQ_LOW  3150
+//#define FREQ_HIGH 6300
+//#define BAUD  630
+
+//#define FREQ_LOW  4900
+//#define FREQ_HIGH 7350
+//#define BAUD  1225
+
+//#define FREQ_LOW  4900
+//#define FREQ_HIGH 7350
+//#define BAUD  2450
