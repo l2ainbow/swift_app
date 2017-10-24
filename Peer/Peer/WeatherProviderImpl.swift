45fc1d3ce4b6023c29b9591ffc4ebfda817e106d
Yu Iijima <y.iijima@nddhq.co.jp>
2017-10-24 20:45:22 +0900

    天気情報取得の実装（途中）

--- Peer/Peer/WeatherProviderImpl.swift
+++ Peer/Peer/WeatherProviderImpl.swift
@@ -1,25 +1,23 @@
-// ˅
-
-// ˄
-
 public class WeatherProviderImpl: WeatherProvider
 {
-    // ˅
-    
-    // ˄
-
+    let MAX_DAYS = 5
+    let OPEN_WEATHER_MAP_URL = "http://atode"
     public func askWeather(daysAgo: Int, location: Location) -> Weather?
     {
-        // ˅
+      if (daysAgo > MAX_DAYS){
         return nil
-        // ˄
+      }
+      let urlStr = OPEN_WEATHER_MAP_URL + "lat=" + location.latitude + "&lon=" location.longitude
+      let url = URL(string: urlStr)
+      let task = URLSession.shared.dataTask(with: url as URL, completionHandler: {data, response, error in
+        if let data = data {
+          let json = JSON(data: data)
+          print(json)
+        }
+      })
+      
+      task.resume()
+      
+        return nil
     }
-
-    // ˅
-    
-    // ˄
 }
-
-// ˅
-
-// ˄
